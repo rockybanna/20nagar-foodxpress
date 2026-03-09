fb5778e16fc76588fdd720b1fcb452112e7f1941
rockybanna <ajayrajpoot420@gmail.com>
2026-03-09 20:04:27 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,40 @@
-const supabase = supabase.createClient(
+const supabaseClient = supabase.createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY
 );
 
-console.log("Connected to Supabase:", SUPABASE_URL);
+async function signup() {
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const { data, error } = await supabaseClient.auth.signUp({
+    email: email,
+    password: password
+  });
+
+  if(error){
+    document.getElementById("message").innerText = error.message;
+  } else {
+    document.getElementById("message").innerText = "Signup successful. Check email.";
+  }
+
+}
+
+async function login(){
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const { data, error } = await supabaseClient.auth.signInWithPassword({
+    email: email,
+    password: password
+  });
+
+  if(error){
+    document.getElementById("message").innerText = error.message;
+  } else {
+    document.getElementById("message").innerText = "Login successful";
+  }
+
+}
