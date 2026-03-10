a9c7622d7f98fcdf8caf764d8f5b86f2b980f460
rockybanna <ajayrajpoot420@gmail.com>
2026-03-10 13:33:04 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // SUPABASE CONFIG
 const SUPABASE_URL = "https://uouwizrknwrqqynodplv.supabase.co";
-const SUPABASE_ANON_KEY = "PASTE_YOUR_ANON_PUBLIC_KEY_HERE";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVvdXdpenJrbndycXF5bm9kcGx2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwNDk1MjksImV4cCI6MjA4ODYyNTUyOX0.oxDZqaT1bMed6uuirN2ZoDCambPYLhOHp2PlhX8gaco";
 
 // CREATE CLIENT
 const supabaseClient = window.supabase.createClient(
